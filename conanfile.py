807b7d53f7d3b90554ab2575a146612d47ed6cc7
Lucas Heer <lucas.heer@online.de>
2023-07-25 20:34:44 +0200

    Update conanfile for conan 2.0

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,16 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.scm import Git
+from conan.tools.files import copy
+
 
 class Sockpp(ConanFile):
     name = "sockpp"
+    version = "0.8.1"
     description = """Modern C++ socket library."""
     license = "BSD-3-Clause License"
     author = "fpagliughi"
     settings = "os", "compiler", "build_type", "arch"
-    exports = "CMakeLists.txt", "src/*", "include/*", "doc/*", "tests/*", "examples/*", "Doxyfile", "version.h.in"
     options = {
         "shared" : [True, False, None],
         "examples" : [True, False, None],
@@ -15,45 +19,31 @@ class Sockpp(ConanFile):
     }
     # If specified None the default values from CMakeLists will be used
     default_options = {
-        "shared" : None,
-        "examples" : None,
-        "tests" : None,
-        "docs" : None
+        "shared" : True,
+        "examples" : False,
+        "tests" : False,
+        "docs" : False
     }
 
-    def set_version(self):
-        git = tools.Git(folder=self.recipe_folder)
-        self.version = git.get_branch()
+    def export_sources(self):
+        files = ["CMakeLists.txt", "src/*", "include/*", "doc/*", "tests/*", "examples/*", "Doxyfile", "cmake/*"]
+        for f in files:
+            copy(self, f, self.recipe_folder, self.export_sources_folder)
 
-    def configure_cmake(self):
-        cmake = CMake(self)
-        # TODO: This might be removed from CMakeLists and BUILD_SHARED_LIBS might be used instead https://docs.conan.io/en/latest/reference/build_helpers/cmake.html#definitions
-        if self.options.shared != None:
-            if self.options.shared:
-                cmake.definitions["SOCKPP_BUILD_SHARED"] = "ON"
-                cmake.definitions["SOCKPP_BUILD_STATIC"] = "OFF"
-            else:
-                cmake.definitions["SOCKPP_BUILD_SHARED"] = "OFF"
-                cmake.definitions["SOCKPP_BUILD_STATIC"] = "ON"
+    def layout(self):
+        cmake_layout(self)
 
-        if self.options.examples != None:
-            cmake.definitions["SOCKPP_BUILD_EXAMPLES"] = "ON" if self.options.examples else "OFF"
-
-        if self.options.tests != None:
-            cmake.definitions["SOCKPP_BUILD_TESTS"] = "ON" if self.options.tests else "OFF"
-
-        if self.options.docs != None:
-            cmake.definitions["SOCKPP_BUILD_DOCUMENTATION"] = "ON" if self.options.docs else "OFF"
-
-        cmake.configure()
-        return cmake
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
@@ -64,3 +54,4 @@ class Sockpp(ConanFile):
             self.cpp_info.system_libs = ["ws2_32"]
         if self.settings.os == "Linux":
             self.cpp_info.libs = ["sockpp"]
+
